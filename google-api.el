d9618faf47215548d9a79b5cb6a71775b96a1cdd
Naoya Yamashita <conao3@gmail.com>
2021-02-13 01:04:44 +0900

    require async-await, request

--- google-api.el
+++ google-api.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Version: 0.0.1
 ;; Keywords: convenience
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "26.1") (async-await "1.1") (request "0.3"))
 ;; URL: https://github.com/conao3/google-api.el
 
 ;; This program is free software: you can redistribute it and/or modify
@@ -27,6 +27,9 @@
 
 
 ;;; Code:
+
+(require 'async-await)
+(require 'request)
 
 (defgroup google-api nil
   "Google API utilities."
